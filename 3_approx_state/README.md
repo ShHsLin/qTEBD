28f886fefc8c67ff717283729416a66f3b343a4d
ShHsLin <shenghsuan.lin@tum.de>
2020-04-03 16:02:30 +0200

    plot

--- 3_approx_state/README.md
+++ 3_approx_state/README.md
@@ -1,13 +1,8 @@
-# qTEBD
 
-qTEBD is the algorithm for real or imaginary time evolution by fidelity maximization. We benchmark such algorithm on MPS for ground state search and quench problem. Then, we show that the algorithm can be generalized to classical simulation of quantum circuit.
-
-
-### Requirement
-
-numpy, jax, (TeNPy)
-
-
+### Circuit
 ![](g1.0.png)
+
+### MPS
+![](mps.png)
 
 
--- 3_approx_state/plot.py
+++ 3_approx_state/plot.py
@@ -6,13 +6,15 @@ sns.set()
 
 if __name__ == '__main__':
     depth = int(sys.argv[1])
+    g = 1.4
+    h = 0.9045
 
-    for idx in range(20,30):
+    for idx in range(1,21):
         try:
-            T = idx * 0.1
-            plt.semilogy(np.load('data/1d_TFI_g1.0/L31/T%.1f/circuit_depth%d_Niter100000_1st_error.npy' % (T, depth)),
+            T = idx * 0.5
+            plt.semilogy(np.load('data/1d_TFI_g%.4f_h%.4f/L31/T%.1f/circuit_depth%d_Niter100000_1st_error.npy' % (g, h, T, depth)),
                          label='T=%.1f' % T)
-            print('plot T %.1f' % T, np.load('data/1d_TFI_g1.0/L31/T%.1f/circuit_depth%d_Niter100000_1st_error.npy' % (T, depth))[0])
+            print('plot T %.1f' % T, np.load('data/1d_TFI_g%.4f_h%.4f/L31/T%.1f/circuit_depth%d_Niter100000_1st_error.npy' % (g, h, T, depth))[0])
         except Exception as e:
             print(e)
 
@@ -25,7 +27,7 @@ if __name__ == '__main__':
     # for idx in range(6):
     #     try:
     #         depth = idx
-    #         plt.semilogy(np.load('data/1d_TFI_g1.0/L31/T%.1f/circuit_depth%d_Niter100000_1st_error.npy' % (T, depth)),
+    #         plt.semilogy(np.load('data/1d_TFI_g%.4f_h%.4f/L31/T%.1f/circuit_depth%d_Niter100000_1st_error.npy' % (T, depth)),
     #                      label='D=%d, T=%.1f' % (depth, T))
     #         print('plot D=%d, T=%.1f' % (depth, T))
     #     except Exception as e:
